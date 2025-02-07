0878b260bf9c4f985bb4cc2579535cfee4306319
Travis Obregon <travisobregon@icloud.com>
2025-02-06 21:28:40 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        Model::shouldBeStrict(! app()->isProduction());
+        Model::unguard();
     }
 }
